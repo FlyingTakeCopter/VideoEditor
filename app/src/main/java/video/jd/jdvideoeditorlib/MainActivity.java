cc10f21e6f1ec8a1d795e4d383e0708b4bbe1b46
liuqikang <liuqikang@jd.com>
2018-06-07 09:44:10 +0800

    提交几个ffmepg命令

--- app/src/main/java/video/jd/jdvideoeditorlib/MainActivity.java
+++ app/src/main/java/video/jd/jdvideoeditorlib/MainActivity.java
@@ -31,19 +31,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     final String path = Environment.getExternalStorageDirectory().getPath() + "/ffmpegtest/";
     String testVideo = "video1.mp4";
+    long duration = VideoUitls.getDuration(path + testVideo);
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.sample_text) {
+            String res;
             // 转码另存
             String cmd = "-i " + path + testVideo + " -acodec copy -vcodec copy -f mp4 " + path + "test_out.mp4";
             // 创建文件拼接视频
             String cmd1 = "-f concat -i " + path + "filelist.txt -c copy " + path + "pj.mp4";
-            // 添加水印
-            String cmd2 = "-i " + path + testVideo +
-                    " -vf \"movie=" +path+"water.jpg"+ " [watermark]; [in][watermark] overlay=10:10 [out]\" wm.mp4";
-//            final String cmd = "-i " + path + testVideo + " " + path + "/test_out.flv";
-            JVEditor.execCmd(cmd2, VideoUitls.getDuration(path + testVideo), new OnEditorListener() {
+            // 添加水印[watermark] 用不了 使用-filter_complex
+//            String cmd2 = "-i " + path + testVideo +
+//                    " -vf \"movie=" +path+"water.jpg"+ " [watermark]; [in][watermark] overlay=10:10 [out]\" wm.mp4";
+            String cmd3 = "-i %s -i %s -filter_complex overlay=0:0 %s";
+            res =  String.format(cmd3, path + testVideo, path+"water.jpg", path + "wm.mp4");
+            // 截取GIF
+            String screenShotCmd = "-i %s -ss %d -t %d -s 320x240 -f gif %s";
+            res = String.format(screenShotCmd, path + testVideo, 0, 4 , path + "test.gif");
+            // 视频截取
+            String cutVideoCmd = "-i %s -ss %d -t %d %s";
+            res = String.format(cutVideoCmd, path + testVideo, 0, 4 , path + "cut.mp4");
+            // 去除声音
+            String removeAudio = "-i %s -vcodec copy -an %s";
+            res = String.format(removeAudio, path + testVideo, path + "noAudio.mp4");
+            // 音频合成（原视频必须没有音频）
+            String addAudio = "-i %s -i %s %s";
+            res = String.format(addAudio, path + testVideo, path+"audio.mp3", path + "addAudio.mp4");
+            JVEditor.execCmd(res, VideoUitls.getDuration(path + testVideo), new OnEditorListener() {
                 @Override
                 public void onSuccess() {
 
