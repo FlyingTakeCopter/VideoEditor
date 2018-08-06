ac93ed3da99800de283a8608a0b4b2b68171f6ec
liuqikang <liuqikang@jd.com>
2018-08-06 14:32:51 +0800

    替换了ffmpeg 3.4.2 的so 增加了控制生成视频质量的指令 -q:v 1 但是生成视频速度问题还是没有解决 准备重新拉分支按照MediaCodec+opengles shader方式

--- app/src/main/java/video/jd/jdvideoeditorlib/MainActivity.java
+++ app/src/main/java/video/jd/jdvideoeditorlib/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     final String path = Environment.getExternalStorageDirectory().getPath() + "/ffmpegtest/";
-    final String testVideo = path + "video1.mp4";
+    final String testVideo = path + "back_1.mp4";
     long duration = VideoUitls.getDuration(path + testVideo);
 
     @Override
@@ -61,26 +61,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String mixAV = "-i %s -i %s -c:v copy -map 0:v:0 -filter_complex [0:a][1:a]amerge=inputs=2[aout] -map [aout] -ac 2 -y %s";
             res = String.format(mixAV, testVideo, path+"audio.mp3", path + "音频混合(在原视频基础上).mp4");
             // 转场黑屏(设置COLOR视频变黑)
-            String blackEnd = "-i %s -filter_complex fade=t=in:st=0:d=3,fade=t=out:st=6:d=3 -y %s";
-            res = String.format(blackEnd, testVideo, path + "转场黑.mp4");
-            // 动图淡入淡出
-            String gif_in_out = "-i %s -i %s -filter_complex [1:v]fade=t=in:st=0:d=1,fade=t=out:st=6:d=0.1[v1];[0:v][v1]overlay=x=0:y=0 %s";
-            res = String.format(gif_in_out, testVideo, path + "timg.gif", path + "动图淡入淡出.mp4");
-            // 调整冷暖色(失败)
-            String s1 = "-i %s -filter_complex geq=r='X/W*r(X,Y)':g='(1-X/W)*g(X,Y)':b='(H-Y)/H*b(X,Y)' %s";
-            res = String.format(s1, testVideo, path + "geq1.mp4");
-            // 画中画效果
-            String viv = "-i %s -i %s -filter_complex [1:v]scale=w=800:h=600:force_original_aspect_ratio=decrease[ckout];[0:v][ckout]overlay=x=W/2-w/2:y=H/2-h/2[out] -map [out] -movflags faststart %s";
-            res = String.format(viv, testVideo, testVideo, path + "画中画.mp4");
-            // 周围模糊效果
-            String mh = "-i %s -filter_complex [0:v]avgblur=sizeX=64:sizeY=0[out1];[0:v]scale=800x600[out2];[out1][out2]overlay=x=50:y=50[outo] -map [outo] -y %s";
-            res = String.format(mh, testVideo, path + "四周模糊.mp4");
-            // 四格播放
-            String fv = "-i %s -i %s -i %s -i %s -filter_complex nullsrc=size=480x640[base];[0:v]setpts=PTS-STARTPTS,scale=240x320[upperleft];[1:v]setpts=PTS-STARTPTS,scale=240x320[upperright];[2:v]setpts=PTS-STARTPTS,scale=240x320[lowerleft];[3:v]setpts=PTS-STARTPTS,scale=240x320[lowerright];[base][upperleft]overlay=shortest=1[tmp1];[tmp1][upperright]overlay=shortest=1:x=240[tmp2];[tmp2][lowerleft]overlay=shortest=1:y=320[tmp3];[tmp3][lowerright]overlay=shortest=1:x=240:y=320 -c:v libx264 %s";
-            res = String.format(fv, testVideo, testVideo, testVideo, testVideo, path + "四格播放.mp4");
-            // 视频滑入
-            String hr = "-i %s -i %s -filter_complex [0:v][1:v]overlay=x='if(gte(t,1),max(0,w-(t-1)*480),NAN)':y=0 -y %s";
-            res = String.format(hr, testVideo, testVideo, path + "视频滑入.mp4");
+            String blackEnd = "-i %s -filter_complex fade=t=in:st=0:d=3,fade=t=out:st=6:d=3 -c:v mpeg4 -q:v 1 -y %s";
+            res = String.format(blackEnd, testVideo, path + "out.mp4");
+//            // 动图淡入淡出
+//            String gif_in_out = "-i %s -i %s -filter_complex [1:v]fade=t=in:st=0:d=1,fade=t=out:st=6:d=0.1[v1];[0:v][v1]overlay=x=0:y=0 %s";
+//            res = String.format(gif_in_out, testVideo, path + "timg.gif", path + "动图淡入淡出.mp4");
+//            // 调整冷暖色(失败)
+//            String s1 = "-i %s -filter_complex geq=r='X/W*r(X,Y)':g='(1-X/W)*g(X,Y)':b='(H-Y)/H*b(X,Y)' %s";
+//            res = String.format(s1, testVideo, path + "geq1.mp4");
+//            // 画中画效果
+//            String viv = "-i %s -i %s -filter_complex [1:v]scale=w=800:h=600:force_original_aspect_ratio=decrease[ckout];[0:v][ckout]overlay=x=W/2-w/2:y=H/2-h/2[out] -map [out] -movflags faststart %s";
+//            res = String.format(viv, testVideo, testVideo, path + "画中画.mp4");
+//            // 周围模糊效果
+//            String mh = "-i %s -filter_complex [0:v]avgblur=sizeX=64:sizeY=0[out1];[0:v]scale=800x600[out2];[out1][out2]overlay=x=50:y=50[outo] -map [outo] -y %s";
+//            res = String.format(mh, testVideo, path + "四周模糊.mp4");
+//            // 四格播放
+//            String fv = "-i %s -i %s -i %s -i %s -filter_complex nullsrc=size=480x640[base];[0:v]setpts=PTS-STARTPTS,scale=240x320[upperleft];[1:v]setpts=PTS-STARTPTS,scale=240x320[upperright];[2:v]setpts=PTS-STARTPTS,scale=240x320[lowerleft];[3:v]setpts=PTS-STARTPTS,scale=240x320[lowerright];[base][upperleft]overlay=shortest=1[tmp1];[tmp1][upperright]overlay=shortest=1:x=240[tmp2];[tmp2][lowerleft]overlay=shortest=1:y=320[tmp3];[tmp3][lowerright]overlay=shortest=1:x=240:y=320 -c:v libx264 %s";
+//            res = String.format(fv, testVideo, testVideo, testVideo, testVideo, path + "四格播放.mp4");
+//            // 视频滑入
+//            String hr = "-i %s -i %s -filter_complex [0:v][1:v]overlay=x='if(gte(t,1),max(0,w-(t-1)*480),NAN)':y=0 -y %s";
+//            res = String.format(hr, testVideo, testVideo, path + "视频滑入.mp4");
 
 
             JVEditor.execCmd(res, VideoUitls.getDuration(testVideo), new OnEditorListener() {
