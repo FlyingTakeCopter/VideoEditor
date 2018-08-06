462d2661638a17878eef19ba8d375c2e46f8e304
liuqikang <liuqikang@jd.com>
2018-08-06 19:41:07 +0800

    增加基础视频播放器及滤镜切换功能

--- app/src/main/java/video/jd/jdvideoeditorlib/MainActivity.java
+++ app/src/main/java/video/jd/jdvideoeditorlib/MainActivity.java
@@ -1,10 +1,13 @@
 package video.jd.jdvideoeditorlib;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.videoeditorsdk.activity.PreviewActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -23,7 +26,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.sample_text) {
-
+            Intent intent=new Intent(MainActivity.this,PreviewActivity.class);
+            intent.putExtra("path","/sdcard/ffmpegtest/1080.mp4");
+            startActivity(intent);
         }
     }
 
